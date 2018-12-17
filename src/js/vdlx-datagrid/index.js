0fe945367ae2e2cbe3acf848c8187ae6da803189
Deniss Detkovs <denissdetkovs@fico.com>
2018-12-17 14:41:13 +0100

    TOM-4302: add modifiesDescendants: false

--- src/js/vdlx-datagrid/index.js
+++ src/js/vdlx-datagrid/index.js
@@ -7,5 +7,6 @@ VDL('vdlx-datagrid', {
     tag: 'vdlx-datagrid',
     attributes: VXDAttributes,
     createViewModel: viewModel,
-    transform: transform
+    transform: transform,
+    modifiesDescendants: false,
 });
