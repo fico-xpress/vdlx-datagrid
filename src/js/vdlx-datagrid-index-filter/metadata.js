eac4af16d5326f6937fbea283abe7f64d30b996a
Tim Hemming <13833731+timhemming-fico@users.noreply.github.com>
2020-05-13 09:51:14 +0100

    TOM4-4859: Updated vdldoc for index filter

--- src/js/vdlx-datagrid-index-filter/metadata.js
+++ src/js/vdlx-datagrid-index-filter/metadata.js
@@ -20,6 +20,34 @@
  */
 export default {
     tag: 'vdlx-datagrid-index-filter',
+    doc: {
+        description: `<p>This tag can be used at either the <code>vdlx-datagrid</code> level or the <code>vdlx-datagrid-column</code>
+            level.</p>
+            
+            <p>When used at the <code>vdlx-datagrid-column</code> level, it can only accept a single value and will filter 
+            the specified index set for the array entity associated with that column. Note that it will not reduce the amount 
+            of data fetched from the server when used at the column level.</p>
+            
+            <p>When used at the <code>vdlx-datagrid</code> level, it can accept one or more values and will filter the specified 
+            index set for all arrays in the table. This will request the filtered data from the server, so reduce the amount
+            of data that is fetched from the server and processed by the table, resulting in improved performance. The filtered 
+            index set will still be shown as a column in the table, so you will need to use <code>vdl-visible="=false"</code>
+            to hide this if necessary.</p>`,
+        example: `
+&lt;!-- Example of an index filter on a column --&gt;
+&lt;vdlx-datagrid&gt;
+    &lt;vdlx-datagrid-column entity=&quot;FactoryDemand&quot; heading=&quot;='Factory Demand (' + vars.filterValue + ')'&quot;&gt;
+        &lt;vdlx-datagrid-index-filter set=&quot;Factories&quot; value=&quot;=vars.filterValue&quot;&gt;&lt;/vdlx-datagrid-index-filter&gt;
+    &lt;/vdlx-datagrid-column&gt;
+&lt;/vdlx-datagrid&gt;
+
+&lt;!-- Example of a server-side index filter on an entire table --&gt;
+&lt;vdlx-datagrid vdl-if=&quot;=vars.multiFilter.length &gt; 0&quot;&gt;
+    &lt;vdlx-datagrid-index-filter set=&quot;MonthsOfYear&quot; value=&quot;=vars.multiFilter&quot;&gt;&lt;/vdlx-datagrid-index-filter&gt;
+    &lt;vdlx-datagrid-column entity=&quot;FactorySupply&quot;&gt;&lt;/vdlx-datagrid-column&gt;
+&lt;/vdlx-datagrid&gt;
+        `
+    },
     requiredAncestor: ['vdlx-datagrid', 'vdlx-datagrid-column'],
     attributes: [
         {
@@ -34,7 +62,8 @@ export default {
         },
         {
             name: 'value',
-            description: 'Single value to fix the indices to.',
+            description: 'Single value to fix the indices to. If used at the <code>vdlx-datagrid</code> level, the value ' +
+                'will accept multiple values in the form of an array or a vdl-var',
             acceptsExpression: true,
             required: true
         }
