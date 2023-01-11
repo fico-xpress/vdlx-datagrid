b55543fd6214691e31376a531c623a811bf76b18
Duncan Simpson <duncansimpson@fico.com>
2023-01-11 12:41:44 +0000

    TBOS-244: adding a custom column sorter that will ensure the totals row is always last

--- src/js/datagrid/custom-data/custom-data-pivot.js
+++ src/js/datagrid/custom-data/custom-data-pivot.js
@@ -59,37 +59,13 @@ function _countIfNotEmpty(a) {
 }
 
 const customSorterTotalsLast = (a, b, aRow, bRow, column, dir, sorterParams) => {
-    //a, b - the two values being compared
-    //aRow, bRow - the row components for the values being compared (useful if you need to access additional fields in the row data for the sort)
-    //column - the column component for the column being sorted
-    //dir - the direction of the sort ("asc" or "desc")
-    //sorterParams - sorterParams object from column definition array
-
-    const totalClassA = aRow.getData().cssClass;//aRow._row.data.cssClass;
+    const totalClassA = aRow.getData().cssClass;
     const totalClassB = bRow.getData().cssClass;
-    const aMinusB = a - b;
-    console.log('--------------------------------------------');
-
-    console.log(`dir: ${dir}: a: ${a} - b: ${b} = ${aMinusB}`);
-
     if (totalClassA || totalClassB) {
-        console.log(`totals row: returning NULL`);
         return null;
-
-        //const aReturn = aMinusB * 100;
-        //console.log(`totals row: returning : ${aReturn}`);
-        // if (dir === 'desc') {
-        //     return Math.abs(aReturn);
-        // } else {
-        //     return aReturn;
-        // }
-
     } else {
-        console.log(`ELSE: ${aMinusB}`);
-        return a - b; //you must return the difference between the two values
+        return a - b;
     }
-
-
 }
 
 
