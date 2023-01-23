31e6cf0b10026a60614d54d6b6ab3b0039a9a99c
Duncan Simpson <duncansimpson@fico.com>
2023-01-23 21:35:06 +0000

    TBOS-244: unit testing transform validation and reinstating skipped tests

--- test/unit/vdlx-pivotgrid/transform.test.js
+++ test/unit/vdlx-pivotgrid/transform.test.js
@@ -2,14 +2,13 @@ import transform from '../../../src/js/vdlx-pivotgrid/transform'
 import {extensionApiMock, componentParamsBuilder} from '../../jest/mocks/extension-api-mock'
 
 import {CUSTOM_COLUMN_DEFINITION} from "../../../src/js/constants";
-import {validateObjectColDefinitions} from "../../../src/js/datagrid/custom-data/custom-column-utils";
 
-describe.skip('vdlx-pivotgrid: transform', () => {
+describe('vdlx-pivotgrid: transform', () => {
 
     let mockElement;
     let apiMock;
     let transformFunction;
-
+    let attrs;
     beforeEach(() => {
 
         mockElement = {
@@ -19,19 +18,69 @@ describe.skip('vdlx-pivotgrid: transform', () => {
             setAttribute: jest.fn(),
             removeAttribute: jest.fn()
         };
+
+        attrs = {
+            data: {
+                name: 'data',
+                expression: {value: 'data'}
+            },
+            'row-set-position': {
+                name: 'pivotRowPositions',
+                expression: {
+                    isString: false,
+                    value: 'pivotRowPositions'
+                }
+            },
+            'row-dimensions': {
+                name: 'pivotRowDimensions',
+                expression: {
+                    isString: false,
+                    value: 'pivotRowDimensions'
+                }
+            },
+            'row-titles': {
+                name: 'pivotRowTitles',
+                expression: {
+                    isString: false,
+                    value: 'pivotRowTitles'
+                }
+            },
+            'column-set-position': {
+                name: 'pivotColumnPositions',
+                expression: {
+                    isString: false,
+                    value: 'pivotColumnPositions'
+                }
+            },
+            'column-dimensions': {
+                name: 'pivotColumnDimensions',
+                expression: {
+                    isString: false,
+                    value: 'pivotColumnDimensions'
+                }
+            },
+            'column-titles': {
+                name: 'pivotColumnTitles',
+                expression: {
+                    isString: false,
+                    value: 'pivotColumnTitles'
+                }
+            }
+        };
+
         apiMock = extensionApiMock;
         transformFunction = transform;
     });
 
 
     it('gets the params builder', () => {
-        transformFunction(mockElement, {}, apiMock);
+        transformFunction(mockElement, attrs, apiMock);
         // gets getComponentParamsBuilder from the api
         expect(apiMock.getComponentParamsBuilder).toBeCalledWith(mockElement);
     });
 
     it('sets default attrs', () => {
-        transformFunction(mockElement, {}, apiMock);
+        transformFunction(mockElement, attrs, apiMock);
 
         // sets column definition type
         expect(componentParamsBuilder.addParam).toBeCalledWith(
@@ -41,35 +90,15 @@ describe.skip('vdlx-pivotgrid: transform', () => {
     });
 
     describe('row attributes', () => {
+        it('throws error when no row attrs set', () => {
+            attrs['row-set-position'] = undefined;
+            attrs['row-dimensions'] = undefined;
+            expect(() => {
+                transformFunction(mockElement, attrs, apiMock);
+            }).toThrow('Error for component vdlx-pivotgrid: Missing row configuration.  Please specify either "row-set-position" or "row-dimensions".');
+        });
 
         it('sets rows attrs', () => {
-            const attrs = {
-                data: {
-                    name: 'data',
-                    expression: {value: 'data'}
-                },
-                'row-set-position': {
-                    name: 'pivotRowPositions',
-                    expression: {
-                        isString: false,
-                        value: 'pivotRowPositions'
-                    }
-                },
-                'row-dimensions': {
-                    name: 'pivotRowDimensions',
-                    expression: {
-                        isString: false,
-                        value: 'pivotRowDimensions'
-                    }
-                },
-                'row-titles': {
-                    name: 'pivotRowTitles',
-                    expression: {
-                        isString: false,
-                        value: 'pivotRowTitles'
-                    }
-                }
-            };
 
             transformFunction(mockElement, attrs, apiMock);
 
@@ -102,13 +131,11 @@ describe.skip('vdlx-pivotgrid: transform', () => {
 
 
         it('row-filter set when expression', () => {
-            const attrs = {
-                'row-filter': {
-                    name: 'rowFilter',
-                    expression: {
-                        isString: false,
-                        value: 'something',
-                    }
+            attrs['row-filter'] = {
+                name: 'rowFilter',
+                expression: {
+                    isString: false,
+                    value: 'something',
                 }
             };
 
@@ -120,50 +147,32 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('row-filter throws error when not expression', () => {
-            const attrs = {
-                'row-filter': {
+            attrs['row-filter'] = {
                     name: 'rowFilter',
                     expression: {
                         isString: true,
                         value: 'something',
                     }
-                }
             };
 
             expect(() => {
                 transformFunction(mockElement, attrs, apiMock);
-            }).toThrow('The vdlx-pivotgrid "row-filter" attribute must be supplied as an expression');
+            }).toThrow('Error for component vdlx-pivotgrid: \"row-filter\" attribute must be supplied as an expression');
 
         });
 
 
     });
     describe('column attributes', () => {
+        it('throws error when no column attrs set', () => {
+            attrs['column-set-position'] = undefined;
+            attrs['column-dimensions'] = undefined;
+            expect(() => {
+                transformFunction(mockElement, attrs, apiMock);
+            }).toThrow('Error for component vdlx-pivotgrid: Missing column configuration.  Please specify either "column-set-position" or "column-dimensions".');
+        });
 
         it('sets column attrs', () => {
-            const attrs = {
-                'column-set-position': {
-                    name: 'pivotColumnPositions',
-                    expression: {
-                        isString: false,
-                        value: 'pivotColumnPositions'
-                    }
-                },
-                'column-dimensions': {
-                    name: 'pivotColumnDimensions',
-                    expression: {
-                        isString: false,
-                        value: 'pivotColumnDimensions'
-                    }
-                },
-                'column-titles': {
-                    name: 'pivotColumnTitles',
-                    expression: {
-                        isString: false,
-                        value: 'pivotColumnTitles'
-                    }
-                }
-            };
 
             transformFunction(mockElement, attrs, apiMock);
 
@@ -189,12 +198,12 @@ describe.skip('vdlx-pivotgrid: transform', () => {
 
     describe('page attrs', () => {
         it('sets page size', () => {
-            const attrs = {
-                'page-size': {
-                    name: 'pageSize',
-                    rawValue: 100,
-                    expression: {isString: true}
-                }
+            attrs['page-size'] = {
+
+                name: 'pageSize',
+                rawValue: 100,
+                expression: {isString: true}
+
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -203,12 +212,10 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('converts page size attr to number and sets', () => {
-            const attrs = {
-                'page-size': {
-                    name: 'pageSize',
-                    rawValue: '100',
-                    expression: {isString: true}
-                }
+            attrs['page-size'] = {
+                name: 'pageSize',
+                rawValue: '100',
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -217,29 +224,11 @@ describe.skip('vdlx-pivotgrid: transform', () => {
 
         });
 
-        it('page size set with expression', () => {
-            const attrs = {
-                'page-size': {
-                    name: 'pageSize',
-                    expression: {
-                        isString: false,
-                        value: 100,
-                    }
-                }
-            };
-
-            transformFunction(mockElement, attrs, apiMock);
-            // not called when pageSize cannot be converted to a number
-            expect(componentParamsBuilder.addParam).toBeCalledWith(attrs['page-size'].name, attrs['page-size'].expression.value);
-        });
-
         it('does not set non numeric page size', () => {
-            const attrs = {
-                'page-size': {
-                    name: 'pageSize',
-                    rawValue: 'one hundred',
-                    expression: {isString: true}
-                }
+            attrs['page-size'] = {
+                name: 'pageSize',
+                rawValue: 'one hundred',
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -248,11 +237,9 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets pagination attrs', () => {
-            const attrs = {
-                'page-mode': {
-                    name: 'pageMode',
-                    rawValue: 'paged'
-                }
+            attrs['page-mode'] = {
+                name: 'pageMode',
+                rawValue: 'paged'
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -261,7 +248,7 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets default pagination attrs', () => {
-            transformFunction(mockElement, {}, apiMock);
+            transformFunction(mockElement, attrs, apiMock);
             // sets pageMode
             expect(componentParamsBuilder.addParam).toBeCalledWith(
                 'pageMode', 'scrolling'
@@ -272,12 +259,10 @@ describe.skip('vdlx-pivotgrid: transform', () => {
     describe('export attributes', () => {
 
         it('sets show export when true', () => {
-            const attrs = {
-                'show-export': {
-                    name: 'showExport',
-                    rawValue: {toUpperCase: jest.fn(()=> 'TRUE')},
-                    expression: {isString: true}
-                }
+            attrs['show-export'] = {
+                name: 'showExport',
+                rawValue: {toUpperCase: jest.fn(() => 'TRUE')},
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -286,12 +271,10 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets show export when false', () => {
-            const attrs = {
-                'show-export': {
-                    name: 'showExport',
-                    rawValue: {toUpperCase: jest.fn(()=> 'not true')},
-                    expression: {isString: true}
-                }
+            attrs['show-export'] = {
+                name: 'showExport',
+                rawValue: {toUpperCase: jest.fn(() => 'not true')},
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -300,13 +283,11 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('export-filename set with expression', () => {
-            const attrs = {
-                'export-filename': {
-                    name: 'exportFilename',
-                    expression: {
-                        isString: false,
-                        value: 'abc.123',
-                    }
+            attrs['export-filename'] = {
+                name: 'exportFilename',
+                expression: {
+                    isString: false,
+                    value: 'abc.123',
                 }
             };
 
@@ -318,13 +299,11 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('export-filename set with raw value', () => {
-            const attrs = {
-                'export-filename': {
-                    name: 'exportFilename',
-                    rawValue: 'abc.123',
-                    expression: {
-                        isString: true
-                    }
+            attrs['export-filename'] = {
+                name: 'exportFilename',
+                rawValue: 'abc.123',
+                expression: {
+                    isString: true
                 }
             };
 
@@ -340,11 +319,9 @@ describe.skip('vdlx-pivotgrid: transform', () => {
     describe('decoration attributes', () => {
 
         it('nulls id on element and sets as param', () => {
-            const attrs = {
-                id: {
-                    name: 'tableId',
-                    rawValue: 'my-table-id'
-                }
+            attrs['id'] = {
+                name: 'tableId',
+                rawValue: 'my-table-id'
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -355,11 +332,9 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets width', () => {
-            const attrs = {
-                width: {
-                    name: 'width',
-                    rawValue: '100'
-                }
+            attrs['width'] = {
+                name: 'width',
+                rawValue: '100'
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -368,11 +343,9 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('removes class on element and sets as param', () => {
-            const attrs = {
-                class: {
-                    name: 'class',
-                    rawValue: 'class-one'
-                }
+            attrs['class'] = {
+                name: 'class',
+                rawValue: 'class-one'
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -383,12 +356,10 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets always-show-selection when true', () => {
-            const attrs = {
-                'always-show-selection': {
-                    name: 'alwaysShowSelection',
-                    rawValue: {toUpperCase: jest.fn(()=> 'TRUE')},
-                    expression: {isString: true}
-                }
+            attrs['always-show-selection'] = {
+                name: 'alwaysShowSelection',
+                rawValue: {toUpperCase: jest.fn(() => 'TRUE')},
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -397,12 +368,10 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets height as string', () => {
-            const attrs = {
-                height: {
-                    name: 'gridHeight',
-                    rawValue: '100',
-                    expression: {isString: true}
-                }
+            attrs['height'] = {
+                name: 'gridHeight',
+                rawValue: '100',
+                expression: {isString: true}
             };
 
             transformFunction(mockElement, attrs, apiMock);
@@ -411,13 +380,11 @@ describe.skip('vdlx-pivotgrid: transform', () => {
         });
 
         it('sets height as expression', () => {
-            const attrs = {
-                height: {
-                    name: 'gridHeight',
-                    expression: {
-                        isString: false,
-                        value: '100'
-                    }
+            attrs['height'] = {
+                name: 'gridHeight',
+                expression: {
+                    isString: false,
+                    value: '100'
                 }
             };
 
